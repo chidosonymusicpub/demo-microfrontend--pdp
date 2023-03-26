44ca21fd20385ca83c08b0a4249b82a5e4daf3cf
cukaigwe <chido.ukaigwe@sonymusicpub.com>
2023-03-26 11:43:29 +0100

    added routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
 import Footer from "home/Footer";
 import Header from "home/Header";
 
 import "./index.scss";
 import SafeComponent from "./SafeComponent";
+import PDPContent from "../PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
+  <BrowserRouter>
+      <div className="text-3xl mx-auto max-w-6xl">
       <Header />
-    </SafeComponent>
-    <div className="my-10">
-      PDP Page Content
+      <div className="my-10">
+      <Routes>
+        <Route path="/product/:id" element={<PDPContent/>} />
+      </Routes>
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
+  </BrowserRouter>
+  
+
+
 );
 ReactDOM.render(<App />, document.getElementById("app"));
